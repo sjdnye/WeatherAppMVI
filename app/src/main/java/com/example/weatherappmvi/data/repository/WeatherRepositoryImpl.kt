096257fe20ea8083431970e7cba1f9b125dd4cfa
sjdnye <sjdnye@gmail.com>
2022-07-18 00:26:02 +0430

    complete locationTracker

---
+++ app/src/main/java/com/example/weatherappmvi/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.example.weatherappmvi.data.repository
+
+import com.example.weatherappmvi.data.mapper.toWeatherInfo
+import com.example.weatherappmvi.data.remote.WeatherApi
+import com.example.weatherappmvi.domain.repository.WeatherRepository
+import com.example.weatherappmvi.domain.util.Resource
+import com.example.weatherappmvi.domain.weather.WeatherInfo
+import java.lang.Exception
+import javax.inject.Inject
+
+class WeatherRepositoryImpl @Inject constructor(
+    private val weatherApi: WeatherApi
+) : WeatherRepository {
+    override suspend fun getWeatherData(lat: Double, long: Double): Resource<WeatherInfo> {
+        return try {
+            Resource.Success(
+                data = weatherApi.getWeatherData(
+                    latitude = lat, longitude = long
+                ).toWeatherInfo()
+            )
+
+        }catch (e: Exception){
+            e.printStackTrace()
+            Resource.Error(e.message ?: "An unknown error occurred")
+        }
+    }
+}
---
+++ app/src/main/java/com/example/weatherappmvi/domain/repository/WeatherRepository.kt
@@ -0,0 +1,11 @@
+package com.example.weatherappmvi.domain.repository
+
+import com.example.weatherappmvi.data.remote.WeatherApi
+import com.example.weatherappmvi.domain.util.Resource
+import com.example.weatherappmvi.domain.weather.WeatherInfo
+
+interface WeatherRepository{
+
+    suspend fun getWeatherData(lat:Double, long:Double): Resource<WeatherInfo>
+
+}
